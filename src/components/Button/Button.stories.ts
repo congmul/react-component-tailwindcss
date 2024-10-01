833493d813dc71d6e69d7f3dfd7974766d046e70
Jehyun Jung <congmul@hotmail.com>
2024-10-01 16:19:15 -0700

    Update Button component

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -14,11 +14,11 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-//   argTypes: {
-//     backgroundColor: { control: 'color' },
-//   },
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-//   args: { onClick: fn() },
+  args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -30,29 +30,3 @@ export const Primary: Story = {
         children: 'Button'
     }
 }
-// export const Primary: Story = {
-//   args: {
-//     primary: true,
-//     label: 'Button',
-//   },
-// };
-
-// export const Secondary: Story = {
-//   args: {
-//     label: 'Button',
-//   },
-// };
-
-// export const Large: Story = {
-//   args: {
-//     size: 'large',
-//     label: 'Button',
-//   },
-// };
-
-// export const Small: Story = {
-//   args: {
-//     size: 'small',
-//     label: 'Button',
-//   },
-// };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import classNames from 'classnames';
 
 export interface ButtonProps {
-    children: React.ReactNode | string
+    /** ReactNode | string */
+    children: React.ReactNode | string;
+     /** 'primary' | 'secondary' */
+    type?: 'primary' | 'secondary';
+    /** 'sm' | 'base' | 'lg' */
+    size?: 'sm' | 'base' | 'lg';
+    className?: string;
+    rounded?: boolean;
+    disabled?: boolean;
+    /** Callback to invoke when the button is clicked */
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button:React.FC<ButtonProps> = ({children}) => {
-    return <button className="bg-sky-500 text-white">{children}</button>
+const Button:React.FC<ButtonProps> = ({children = 'Button', type = 'primary', size = 'base', className, rounded = false, disabled = false, onClick, ...restProps}) => {
+    const buttonStyles = {
+        primary: "bg-sky-500 text-white hover:bg-sky-400 border border-sky-500",
+        secondary: "bg-white text-sky-500 hover:bg-sky-100 border border-sky-500",
+    }
+    const buttonSize = {
+        sm: "py-2 px-5 text-sm",
+        base: "py-2 px-6 text-base",
+        lg: "py-2 px-7 text-lg"
+    }
+    const disabledType = {
+        primary: 'disabled:cursor-not-allowed disabled:bg-sky-300 disabled:border-sky-300',
+        secondary: 'disabled:cursor-not-allowed disabled:bg-white disabled:text-sky-300 disabled:border-sky-300',
+    }
+    const buttonClass = classNames(
+        buttonSize[size],
+        rounded ? 'rounded-3xl' : 'rounded',
+        buttonStyles[type],
+        disabledType[type],
+        className
+    );
+    return <button 
+            className={buttonClass}
+            role="button"
+            type="button"
+            onClick={onClick}
+            disabled={disabled}
+            {...restProps}
+        >{children}</button>
 }
 
 export default Button;
