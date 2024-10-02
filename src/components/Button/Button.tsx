fe3e3e9de7f990dccb39a6c2af83389a744df1e5
Jehyun Jung <congmul@hotmail.com>
2024-10-02 09:02:29 -0700

    Add color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,8 @@ export interface ButtonProps {
     /** ReactNode | string */
     children: React.ReactNode | string;
     /** 'primary' | 'secondary' */
-    variant?: 'primary' | 'secondary';
+    variant?: 'primary' | 'secondary' | 'text';
+    color?: 'sky' | 'red' | 'green' | 'amber' | 'pink';
     /** 'button' | 'submit' | 'reset' */
     type?: 'button' | 'submit' | 'reset';
     /** 'sm' | 'base' | 'lg' */
@@ -17,10 +18,38 @@ export interface ButtonProps {
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button:React.FC<ButtonProps> = ({children = 'Button', variant = 'primary', size = 'base', type = 'button', className, rounded = false, disabled = false, onClick, ...restProps}) => {
+const Button:React.FC<ButtonProps> = ({children = 'Button', variant = 'primary', color = 'sky',  size = 'base', type = 'button', className, rounded = false, disabled = false, onClick, ...restProps}) => {
     const buttonStyles = {
-        primary: "bg-sky-500 text-white hover:bg-sky-400 border border-sky-500",
-        secondary: "bg-white text-sky-500 hover:bg-sky-100 border border-sky-500",
+        primary: 'text-white border',
+        secondary: 'bg-white border',
+        text: 'bg-white'
+    }
+    const buttonColor = {
+        sky: {
+            primary: `${buttonStyles[variant]} bg-sky-500 hover:bg-sky-400 border-sky-500`,
+            secondary: `${buttonStyles[variant]} text-sky-500 hover:bg-sky-100 border-sky-500`,
+            text: `${buttonStyles[variant]} text-sky-500 hover:bg-sky-100 border-sky-500`,
+        },
+        red: {
+            primary: `${buttonStyles[variant]} bg-red-500 hover:bg-red-400 border-red-500`,
+            secondary: `${buttonStyles[variant]} text-red-500 hover:bg-red-100 border-red-500`,
+            text: `${buttonStyles[variant]} text-red-500 hover:bg-red-100 border-red-500`,
+        },
+        green: {
+            primary: `${buttonStyles[variant]} bg-green-500 hover:bg-green-400 border-green-500`,
+            secondary: `${buttonStyles[variant]} text-green-500hover:bg-green-100 border-green-500`,
+            text: `${buttonStyles[variant]} text-green-500 hover:bg-green-100 border-green-500`,
+        },
+        amber: {
+            primary: `${buttonStyles[variant]} bg-amber-500 hover:bg-amber-400 border-amber-500`,
+            secondary: `${buttonStyles[variant]} text-amber-500 hover:bg-amber-100 border-amber-500`,
+            text: `${buttonStyles[variant]} text-amber-500 hover:bg-amber-100 border-amber-500`,
+        },
+        pink: {
+            primary: `${buttonStyles[variant]} bg-red-300 hover:bg-red-400 border-red-300`,
+            secondary: `${buttonStyles[variant]} text-red-300 hover:bg-red-50 border-red-300`,
+            text: `${buttonStyles[variant]} text-red-300 hover:bg-red-50 border-red-300`,
+        },
     }
     const buttonSize = {
         sm: "py-2 px-5 text-sm",
@@ -28,14 +57,37 @@ const Button:React.FC<ButtonProps> = ({children = 'Button', variant = 'primary',
         lg: "py-2 px-7 text-lg"
     }
     const disabledType = {
-        primary: 'disabled:cursor-not-allowed disabled:bg-sky-300 disabled:border-sky-300',
-        secondary: 'disabled:cursor-not-allowed disabled:bg-white disabled:text-sky-300 disabled:border-sky-300',
+        sky: {
+            primary: `disabled:cursor-not-allowed disabled:bg-sky-300 disabled:border-sky-300`,
+            secondary: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-sky-300 disabled:border-sky-300`,
+            text: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-sky-300`,
+        },
+        red: {
+            primary: `disabled:cursor-not-allowed disabled:bg-red-300 disabled:border-red-300`,
+            secondary: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-red-300 disabled:border-red-300`,
+            text: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-red-300`,
+        },
+        green: {
+            primary: `disabled:cursor-not-allowed disabled:bg-green-300 disabled:border-green-300`,
+            secondary: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-green-300 disabled:border-green-300`,
+            text: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-green-300`,
+        },
+        amber: {
+            primary: `disabled:cursor-not-allowed disabled:bg-amber-300 disabled:border-amber-300`,
+            secondary: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-amber-300 disabled:border-amber-300`,
+            text: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-amber-300`,
+        },
+        pink: {
+            primary: `disabled:cursor-not-allowed disabled:bg-red-100 disabled:border-red-100`,
+            secondary: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-red-100 disabled:border-red-100`,
+            text: `disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:text-red-100`,
+        },
     }
     const buttonClass = classNames(
         buttonSize[size],
         rounded ? 'rounded-3xl' : 'rounded',
-        buttonStyles[variant],
-        disabledType[variant],
+        buttonColor[color][variant],
+        disabledType[color][variant],
         className
     );
     return <button 
