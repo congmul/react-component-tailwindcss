67b6af5ef576534ee0efc5ccf8571494d66b7bc5
Jehyun Jung <congmul@hotmail.com>
2024-10-03 16:55:33 -0700

    Dynamic Code snippets for Button

---
+++ src/components/Drawer/Drawer.storiesTest.tsx
@@ -0,0 +1,23 @@
+// import type { Meta, StoryObj } from '@storybook/react';
+ 
+// import Drawer from './Drawer';
+// import React from 'react';
+// import { Title, Subtitle, Description, Primary as Primary2, Controls, Stories } from "@storybook/blocks";
+ 
+// const meta: Meta<typeof Drawer> = {
+//   // title: 'Components/Drawer',
+//   component: Drawer,
+//   // tags: ['autodocs'],
+//   // parameters: {
+//   //   layout: 'centered',
+//   // }
+// };
+ 
+// export default meta;
+// type Story = StoryObj<typeof Drawer>;
+
+// export const Primary: Story = {
+//   args: {
+//     children: 'Drawer',
+//   },
+// };
--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import classNames from 'classnames';
 
 export interface DrawerProps {
+    children: React.ReactNode | string;
+    isOpen: boolean
 }
 
-const Drawer:React.FC<DrawerProps> = ({}) => {
+const Drawer:React.FC<DrawerProps> = ({children, isOpen = false}) => {
     const DrawerClass = classNames(
-        'Drawer'
+        // 'fixed transition-transform transform z-10 inset-y-0 left-0',
+        'fixed top-0 left-0 h-full bg-gray-800 text-white transform transition-transform duration-300 ease-in-out',
+        isOpen ? 'translate-x-0' : '-translate-x-full'
     );
     return(
-        <div className={DrawerClass}>
-            Drawer
+        <div className={DrawerClass} style={{ width: '250px' }}>
+            {children}
         </div>)
 }
 export default Drawer;
