aab1f073bc309aad65809e3f489a0bd0aa734c5b
Jehyun Jung <congmul@hotmail.com>
2024-10-04 08:21:12 -0700

    Update Button and Spinner

--- src/components/Spinner/Spinner.stories.ts
+++ src/components/Spinner/Spinner.stories.ts
@@ -41,8 +41,4 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
-//     args: {
-//         children: 'Spinner'
-//     }
-}
+export const Main: Story = {}
