ae9afee11383f1800474e08d7a6fab555cf9f7f6
Jehyun Jung <congmul@hotmail.com>
2026-03-09 17:44:25 -0700

    Add tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 // postcss.config.js
 module.exports = {
-    plugins: {
-      // Tailwind v4 moved the PostCSS plugin to a separate package.
-      // See https://tailwindcss.com/docs/installation for details.
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  };
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};
